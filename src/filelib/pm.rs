d6a81fac809e2450b0ef81624f6561aefb93551e
Mohaned Mohammed Sherhan <shrhanmhnd15@gmail.com>
2025-05-14 20:12:56 +0300

    Added filelib test

--- src/filelib/pm.rs
+++ src/filelib/pm.rs
@@ -49,3 +49,45 @@ pub fn warning_encrypt_database() {
         logger.warning("please use 'password-manager encrypt' to encrypt it!");
     }
 }
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn get_encrypted_db_path() {
+        let db_path = super::get_encrypted_db_path();
+        assert_eq!(
+            db_path,
+            super::data_dir()
+                .expect("can NOT get the system data directory path!!")
+                .join("XPManager/data/passwords.db.x"),
+            "Encryption path NOT match!!"
+        );
+    }
+
+    #[test]
+    fn get_decrypted_db_path() {
+        let db_path = super::get_decrypted_db_path();
+        assert_eq!(
+            db_path,
+            super::data_dir()
+                .expect("can NOT get the system data directory path!!")
+                .join("XPManager/data/passwords.db"),
+            "Encryption path NOT match!!"
+        );
+    }
+
+    #[test]
+    fn db_state() {
+        let en_db_path = super::get_encrypted_db_path();
+        let de_db_path = super::get_decrypted_db_path();
+        let state = if en_db_path.exists() {
+            super::FileState::Encrypted
+        } else if de_db_path.exists() {
+            super::FileState::Decrypted
+        } else {
+            super::FileState::NotFound
+        };
+        let result = state == super::db_state();
+        assert_eq!( result, true, "Password manager database state NOT match!!" );
+    }
+}
